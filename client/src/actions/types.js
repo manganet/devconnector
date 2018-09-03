050727eca323432a37127bd05202a1dbcfcd1b72
manganet <manganet55@gmail.com>
2018-09-03 17:28:04 +0400

    getPosts Action & PostFeed Component

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -7,5 +7,6 @@ export const CLEAR_CURRENT_PROFILE = "CLEAR_CURRENT_PROFILE";
 export const GET_PROFILES = "GET_PROFILES";
 export const POST_LOADING = "POST_LOADING";
 export const GET_POST = "GET_POST";
+export const GET_POSTS = "GET_POSTS";
 export const ADD_POST = "ADD_POST";
 export const DELETE_POST = "DELETE_POST";
